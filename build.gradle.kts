d439c34f0dfbc15d4c3f593ec45d1a12041e6ba5
supermonkeygodkingemperor <ntsouls@gmail.com>
2024-10-10 21:37:19 -0700

    idk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
+plugins {
+    id("com.android.application") version "8.1.0" apply false // Replace with your desired Android Gradle plugin version
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false // Replace with your desired Kotlin Gradle plugin version
+}
+
 android {
-    // ...
+    // ... your existing android configurations
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
         }
     }
+}
+
+dependencies {
+    // ... your existing dependencies
 }
